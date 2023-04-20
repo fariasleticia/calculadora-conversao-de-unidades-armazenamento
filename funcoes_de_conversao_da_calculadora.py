6ce9d81e11c31acd8baf5e4111a87d7689e7aae7
unknown <leticiafcassimiro@gmail.com>
2023-04-20 10:33:38 -0300

    Criação de duas funções de conversão, convertendo o valor do input float de Byte para Kilobyte e vice-versa.

--- funcoes_de_conversao_da_calculadora.py
+++ funcoes_de_conversao_da_calculadora.py
@@ -1,3 +1,5 @@
+CONSTANTE_DE_CONVERSAO = 1024
+
 #funções de conversão:
 
 def bit_para_byte(numero_inserido):
@@ -7,6 +9,14 @@ def bit_para_byte(numero_inserido):
 def byte_para_bit(numero_inserido):
     bits_calculado = numero_inserido * 8
     return bits_calculado
+
+def byte_para_kb(numero_inserido):
+    kb_calculado = numero_inserido / CONSTANTE_DE_CONVERSAO
+    return kb_calculado
+
+def kb_para_byte(numero_inserido):
+    byte_calculado = numero_inserido * CONSTANTE_DE_CONVERSAO
+    return byte_calculado
 
 #prints das conversões acima:
 
@@ -18,4 +28,14 @@ print('Valor convertido:', valor_convertido)
 print('Insira o valor a ser convertido de Byte para bit:')
 numero_inserido = float(input())
 valor_convertido = byte_para_bit(numero_inserido)
+print('Valor convertido:', valor_convertido)
+
+print('Insira o valor a ser convertido de Byte para Kilobyte:')
+numero_inserido = float(input())
+valor_convertido = byte_para_kb(numero_inserido)
+print('Valor convertido:', valor_convertido)
+
+print('Insira o valor a ser convertido de Kilobyte para Byte:')
+numero_inserido = float(input())
+valor_convertido = kb_para_byte(numero_inserido)
 print('Valor convertido:', valor_convertido)
