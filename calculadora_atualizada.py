d9f81feee08e65b6dc420458189969a2f6d36738
unknown <leticiafcassimiro@gmail.com>
2023-04-27 11:19:33 -0300

    A função de cálculo de conversão dos número foi complementada com uma estrutura condicional capaz de converter os valores para bit e vice-versa, criando três condicionais para o caso de bit ser a unidade inicial, final ou ambas. Para isso, primeiro o valor é transformado em byte e, em seguida, em bit ou no valor final, a depender da posição das unidades.

--- calculadora_atualizada.py
+++ calculadora_atualizada.py
@@ -25,12 +25,24 @@ numero_inserido = int(input("Valor a ser convertido: "))
 
 #conversao do valor inserido
 def calculo_conversao():
-    if posicao_inc >= posicao_fnl:
-        distancia = posicao_inc - posicao_fnl
-        resultado = numero_inserido * (VALOR_CONV_PRINCIPAL**distancia)
-    elif posicao_inc < posicao_fnl:
-        distancia = posicao_fnl - posicao_inc
-        resultado = numero_inserido / (VALOR_CONV_PRINCIPAL**distancia)
+    if unidade_inicial == 'bit' and unidade_final != 'bit':
+        conversao_primaria = numero_inserido / VALOR_CONV_BIT
+        distancia = posicao_fnl - unidades_de_armazenamento.index('byte')
+        resultado = conversao_primaria / (VALOR_CONV_PRINCIPAL**distancia)
+    elif unidade_final == 'bit' and unidade_inicial != 'bit':
+        distancia = posicao_inc - unidades_de_armazenamento.index('byte')
+        conversao_primaria = numero_inserido * (VALOR_CONV_PRINCIPAL**distancia)
+        resultado = conversao_primaria * VALOR_CONV_BIT
+    elif unidade_final == 'bit' and unidade_inicial == 'bit':
+        resultado = numero_inserido
+
+    else:
+        if posicao_inc >= posicao_fnl:
+            distancia = posicao_inc - posicao_fnl
+            resultado = numero_inserido * (VALOR_CONV_PRINCIPAL**distancia)
+        elif posicao_inc < posicao_fnl:
+            distancia = posicao_fnl - posicao_inc
+            resultado = numero_inserido / (VALOR_CONV_PRINCIPAL**distancia)
     print('\nOkay. Seu resultado é:', resultado, str(unidade_final) + '(s)')
 
 calculo_conversao()
