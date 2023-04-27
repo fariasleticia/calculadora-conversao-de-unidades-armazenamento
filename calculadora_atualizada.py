fa59f0489e48d685cec1ccaae389ea5be20bfe54
unknown <leticiafcassimiro@gmail.com>
2023-04-27 09:04:35 -0300

    Adição da funcionalidade de validação dos dados inseridos pelo usuário, através do uso do loop while, e também o cálculo da posição das unidades na lista de armazenamento inicial, utilizando o index, a fim de utilizar o valor da posição futuramente nos cálculos de conversão.

--- calculadora_atualizada.py
+++ calculadora_atualizada.py
@@ -8,7 +8,17 @@ print('Olá! Seja bem-vinda à minha humilde calculadora :)')
 print('\nEstas são as unidades de armazenamento que você poderá usar aqui:', unidades_de_armazenamento)
 print('\nPor favor, insira os valores pedidos abaixo.')
 
-#coleta dos dados
+#coleta dos dados e calculo das distancias
 unidade_inicial = input("\nUnidade inicial: ")
+while unidade_inicial not in unidades_de_armazenamento:
+    unidade_inicial = input("Unidade inválida. Insira alguma presente na lista: ")
+else:
+    posicao_inc = unidades_de_armazenamento.index(unidade_inicial)
+
 unidade_final = input("Unidade final: ")
+while unidade_final not in unidades_de_armazenamento:
+    unidade_final = input("Unidade inválida. Insira alguma presente na lista: ")
+else:
+    posicao_fnl = unidades_de_armazenamento.index(unidade_final)
+
 numero_inserido = int(input("Valor a ser convertido: "))
