aff5a2ecc4abeff6a182bc37c7bd18169ee70c4e
unknown <leticiafcassimiro@gmail.com>
2023-04-27 13:44:28 -0300

    Finalização da calculadora de conversão de unidades de armazenamento, criando um loop para repetir o programa indefinidamente, de acordo com o usuário. Para isso, os códigos de conversão e coleta de dados foram unidos em uma função, a qual foi aplicada na estrutura de repetição while com uso de condicionais.

--- calculadora_atualizada.py
+++ calculadora_atualizada.py
@@ -8,41 +8,56 @@ print('Olá! Seja bem-vinda à minha humilde calculadora :)')
 print('\nEstas são as unidades de armazenamento que você poderá usar aqui:', unidades_de_armazenamento)
 print('\nPor favor, insira os valores pedidos abaixo.')
 
-#coleta dos dados e calculo das distancias
-unidade_inicial = input("\nUnidade inicial: ")
-while unidade_inicial not in unidades_de_armazenamento:
-    unidade_inicial = input("Unidade inválida. Insira alguma presente na lista: ")
-else:
-    posicao_inc = unidades_de_armazenamento.index(unidade_inicial)
-
-unidade_final = input("Unidade final: ")
-while unidade_final not in unidades_de_armazenamento:
-    unidade_final = input("Unidade inválida. Insira alguma presente na lista: ")
-else:
-    posicao_fnl = unidades_de_armazenamento.index(unidade_final)
-
-numero_inserido = int(input("Valor a ser convertido: "))
-
-#conversao do valor inserido
-def calculo_conversao():
-    if unidade_inicial == 'bit' and unidade_final != 'bit':
-        conversao_primaria = numero_inserido / VALOR_CONV_BIT
-        distancia = posicao_fnl - unidades_de_armazenamento.index('byte')
-        resultado = conversao_primaria / (VALOR_CONV_PRINCIPAL**distancia)
-    elif unidade_final == 'bit' and unidade_inicial != 'bit':
-        distancia = posicao_inc - unidades_de_armazenamento.index('byte')
-        conversao_primaria = numero_inserido * (VALOR_CONV_PRINCIPAL**distancia)
-        resultado = conversao_primaria * VALOR_CONV_BIT
-    elif unidade_final == 'bit' and unidade_inicial == 'bit':
-        resultado = numero_inserido
-
+def calculadora_dados_e_conversao():
+    #coleta dos dados e calculo das distancias
+    unidade_inicial = input("\nUnidade inicial: ")
+    while unidade_inicial not in unidades_de_armazenamento:
+        unidade_inicial = input("Unidade inválida. Insira alguma presente na lista: ")
     else:
-        if posicao_inc >= posicao_fnl:
-            distancia = posicao_inc - posicao_fnl
-            resultado = numero_inserido * (VALOR_CONV_PRINCIPAL**distancia)
-        elif posicao_inc < posicao_fnl:
-            distancia = posicao_fnl - posicao_inc
-            resultado = numero_inserido / (VALOR_CONV_PRINCIPAL**distancia)
-    print('\nOkay. Seu resultado é:', resultado, str(unidade_final) + '(s)')
+        posicao_inc = unidades_de_armazenamento.index(unidade_inicial)
 
-calculo_conversao()
+    unidade_final = input("Unidade final: ")
+    while unidade_final not in unidades_de_armazenamento:
+        unidade_final = input("Unidade inválida. Insira alguma presente na lista: ")
+    else:
+        posicao_fnl = unidades_de_armazenamento.index(unidade_final)
+    
+    numero_inserido = int(input("Valor a ser convertido: "))
+
+    #conversao do valor inserido
+    def calculo_conversao():
+        if unidade_inicial == 'bit' and unidade_final != 'bit':
+            conversao_primaria = numero_inserido / VALOR_CONV_BIT
+            distancia = posicao_fnl - unidades_de_armazenamento.index('byte')
+            resultado = conversao_primaria / (VALOR_CONV_PRINCIPAL**distancia)
+        elif unidade_final == 'bit' and unidade_inicial != 'bit':
+            distancia = posicao_inc - unidades_de_armazenamento.index('byte')
+            conversao_primaria = numero_inserido * (VALOR_CONV_PRINCIPAL**distancia)
+            resultado = conversao_primaria * VALOR_CONV_BIT
+        elif unidade_final == 'bit' and unidade_inicial == 'bit':
+            resultado = numero_inserido
+
+        else:
+            if posicao_inc >= posicao_fnl:
+                distancia = posicao_inc - posicao_fnl
+                resultado = numero_inserido * (VALOR_CONV_PRINCIPAL**distancia)
+            elif posicao_inc < posicao_fnl:
+                distancia = posicao_fnl - posicao_inc
+                resultado = numero_inserido / (VALOR_CONV_PRINCIPAL**distancia)
+        print('\nOkay. Seu resultado é:', resultado, str(unidade_final) + '(s)')
+    calculo_conversao()
+
+calculadora_dados_e_conversao()
+
+#looping de repeticao da calculadora
+parar_programa = False
+
+while parar_programa == False:
+    print("\nQuer calcular mais alguma coisa? Insira 'sim' ou 'nao'.")
+    repetir_programa = input()
+    if repetir_programa == 'sim':
+        print('\njoinha!\nInsira os valores pedidos abaixo:')
+        calculadora_dados_e_conversao()
+    elif repetir_programa == 'nao':
+        print('\nbelezinha <3')
+        parar_programa = True
